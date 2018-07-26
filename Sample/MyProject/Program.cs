56f4587b04fd3e22d4ab0f5f38094d24acf7f7e5
ngohungphuc <ngohungphuc95@gmail.com>
2018-07-26 11:53:00 +0700

    add new sample code & book

--- Sample/MyProject/Program.cs
+++ Sample/MyProject/Program.cs
@@ -7,7 +7,16 @@ namespace MyProject
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+            Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+            // generate a random private key
+            Key privateKey = new Key();
+            //From the private key, we use a one-way cryptographic function, to generate a public key.
+            PubKey publicKey = privateKey.PubKey;
+            Console.WriteLine(publicKey);
+            //we can easily get your bitcoin address from your public key
+            // and the network on which this address should be used.
+            Console.WriteLine(publicKey.GetAddress(Network.Main));
+            Console.WriteLine(publicKey.GetAddress(Network.TestNet));
         }
     }
 }
